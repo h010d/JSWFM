18f7b0db99d2795e477e545913265697f38388b1
h010d <lessonsep@gmail.com>
2018-12-11 00:50:55 +0200

    serty

---
+++ 01/js/script.js
@@ -0,0 +1,23 @@
+var my_var = 10;
+var i = 47;
+var a = 10;
+
+var b = 10;
+var c = '20';
+ var h = 0.09;
+ var j = 0.1;
+ console.log(h, j);
+/*10, 20
+'hello',"hello"
+true false
+
+null
+undefined*/
+
+c = 45;
+a = c + b; // 'hello' + 'world ' = 'helloworld'
+alert(a);
+
+
+
+//stringdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfg
